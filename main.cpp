69e559678c711057b1122a9e9a47e6175b91e266
pianoest <wxnb666nb@outlook.com>
2026-07-22 16:19:23 +0800

    tst

---
+++ main.cpp
@@ -0,0 +1,9 @@
+//
+// Created by WX on 2026/7/22.
+//
+
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+
+}
---
+++ main.h
@@ -0,0 +1,16 @@
+//
+// Created by WX on 2026/7/22.
+//
+
+#ifndef MAIN_H
+#define MAIN_H
+
+
+
+class main {
+
+};
+
+
+
+#endif //MAIN_H
