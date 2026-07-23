bf5ef286c8a608906c1c236cee15912bc97e1c1e
pianoest <wxnb666nb@outlook.com>
2026-07-23 19:53:30 +0800

    重叠区间

--- main.cpp
+++ main.cpp
@@ -1,9 +1,34 @@
-//
-// Created by WX on 2026/7/22.
-//
-
 #include <bits/stdc++.h>
 using namespace std;
-int main() {
 
+int main()
+{
+      string s;
+      getline(cin, s);
+
+      int a[26] = {0};
+      for (int i = 0; i < s.length(); i++)
+            a[s[i]-'a'] = i;
+
+      string t;
+      vector<bool> v(26, false);
+
+      for (int i = 0; i < s.length(); i++)
+      {
+            if (v[s[i]-'a'])
+                  continue;
+
+            while (!t.empty() &&
+                   t.back() > s[i] &&
+                   a[t.back()-'a'] > i)
+            {
+                  v[t.back()-'a'] = false;
+                  t.pop_back();
+            }
+
+            t.push_back(s[i]);
+            v[s[i]-'a'] = true;
+      }
+
+      cout << t << endl;
 }
