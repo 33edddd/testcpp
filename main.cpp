55776011d67dcdde0b8ccf81a8d1c2f27817b378
pianoest <wxnb666nb@outlook.com>
2026-07-29 20:27:40 +0800

    拼接最大数（不会）

--- main.cpp
+++ main.cpp
@@ -1,47 +1,122 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int main()
-{
-        int n;
-        cin >> n;
+class Solution {
+public:
 
-        vector<int> nums(n);
-        unordered_map<int,int> cnt;
-        unordered_map<int,int> tail;
-
-        for(int i=0;i<n;i++)
-        {
-                cin>>nums[i];
-                cnt[nums[i]]++;
-        }
+    // 获取长度为k的最大子序列
+    vector<int> getMax(vector<int>& nums,int k)
+    {
+        vector<int> st;
+        int remove=nums.size()-k;
 
         for(int x:nums)
         {
-                if(cnt[x]==0)
-                        continue;
-
-                if(tail[x-1]>0)
-                {
-                        tail[x-1]--;
-                        tail[x]++;
-                        cnt[x]--;
-                }
-                else if(cnt[x+1]>0 && cnt[x+2]>0)
-                {
-                        cnt[x]--;
-                        cnt[x+1]--;
-                        cnt[x+2]--;
-                        tail[x+2]++;
-                }
-                else
-                {
-                        cout<<"false";
-                        return 0;
-                }
+            while(!st.empty() && remove>0 && st.back()<x)
+            {
+                st.pop_back();
+                remove--;
+            }
+            st.push_back(x);
         }
 
-        cout<<"true";
+        st.resize(k);
 
-        return 0;
+        return st;
+    }
+
+
+    // 合并两个数组
+    vector<int> merge(vector<int>& a,vector<int>& b)
+    {
+        vector<int> res;
+
+        int i=0,j=0;
+
+        while(i<a.size() || j<b.size())
+        {
+            if(compare(a,i,b,j))
+                res.push_back(a[i++]);
+            else
+                res.push_back(b[j++]);
+        }
+
+        return res;
+    }
+
+
+    // 判断a[i...]是否大于b[j...]
+    bool compare(vector<int>& a,int i,vector<int>& b,int j)
+    {
+        while(i<a.size() && j<b.size())
+        {
+            if(a[i]!=b[j])
+                return a[i]>b[j];
+
+            i++;
+            j++;
+        }
+
+        return (a.size()-i)>(b.size()-j);
+    }
+
+
+    vector<int> maxNumber(vector<int> &nums1, vector<int> &nums2,int k)
+    {
+        vector<int> ans;
+
+        for(int i=0;i<=k;i++)
+        {
+            if(i<=nums1.size() && k-i<=nums2.size())
+            {
+                vector<int> a=getMax(nums1,i);
+                vector<int> b=getMax(nums2,k-i);
+
+                vector<int> temp=merge(a,b);
+
+                if(ans.empty() || compare(temp,0,ans,0))
+                    ans=temp;
+            }
+        }
+
+        return ans;
+    }
+};
+
+
+int main()
+{
+    int m,n,k,data;
+
+    vector<int> nums1,nums2;
+
+    cin>>m;
+
+    for(int i=0;i<m;i++)
+    {
+        cin>>data;
+        nums1.push_back(data);
+    }
+
+
+    cin>>n;
+
+    for(int i=0;i<n;i++)
+    {
+        cin>>data;
+        nums2.push_back(data);
+    }
+
+
+    cin>>k;
+
+
+    vector<int> res=Solution().maxNumber(nums1,nums2,k);
+
+
+    for(int i=0;i<res.size();i++)
+        cout<<res[i];
+
+
+    return 0;
 }
