8c978ed5df4322b6ec0a474f13279069925f5b25
pianoest <wxnb666nb@outlook.com>
2026-08-10 17:36:14 +0800

    组合总和(要会)

--- main.cpp
+++ main.cpp
@@ -1,93 +1,49 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+class Solution {
+public:
 
-// 回溯函数
-bool dfs(vector<int>& nums, vector<bool>& used, int k, int start, int sum, int target)
-{
-    // 已经找到 k-1 个满足条件的子集
-    // 剩下的数字一定组成最后一个子集
-    if (k == 1)
-        return true;
-
-
-    // 当前子集和满足要求，开始寻找下一个子集
-    if (sum == target)
+    void dfs(vector<int>& candidates,
+             int target,
+             int start,
+             vector<int>& path,
+             vector<vector<int>>& res)
     {
-        return dfs(nums, used, k - 1, 0, 0, target);
+        if (target ==0) {
+            res.push_back(path);
+            return;
+        }
+        for (int i = start; i < candidates.size(); i++) {
+
+            if (candidates[i]>target) {
+                break;
+            }
+            path.push_back(candidates[i]);
+            dfs(candidates,target-candidates[i],i,path,res);
+            path.pop_back();
+        }
     }
 
 
-    for (int i = start; i < nums.size(); i++)
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target)
     {
-        if (used[i])
-            continue;
+        vector<vector<int>> res;
+
+        vector<int> path;
 
 
-        // 当前数字加入后超过目标
-        if (sum + nums[i] > target)
-            continue;
+        // 排序用于剪枝
+        sort(candidates.begin(), candidates.end());
 
 
-        used[i] = true;
+        dfs(candidates, target, 0, path, res);
 
 
-        if (dfs(nums, used, k, i + 1, sum + nums[i], target))
-            return true;
-
-
-        // 回溯
-        used[i] = false;
-
-
-        // 如果当前桶为空，放当前数字失败，
-        // 后面更小的数字也不需要尝试
-        if (sum == 0)
-            break;
-
-
-        // 跳过重复数字
-        while (i + 1 < nums.size() && nums[i] == nums[i + 1])
-            i++;
+        return res;
     }
-
-
-    return false;
-}
-
-
-
-// 判断是否可以划分为 k 个和相等的子集
-bool canPartitionKSubsets(vector<int>& nums, int k)
-{
-    int total = 0;
-
-    for (int x : nums)
-        total += x;
-
-
-    // 总和不能被 k 整除
-    if (total % k != 0)
-        return false;
-
-
-    int target = total / k;
-
-
-    // 从大到小排序，提高剪枝效率
-    sort(nums.begin(), nums.end(), greater<int>());
-
-
-    // 最大元素超过目标，不可能
-    if (nums[0] > target)
-        return false;
-
-
-    vector<bool> used(nums.size(), false);
-
-
-    return dfs(nums, used, k, 0, 0, target);
-}
+};
 
 
 
@@ -97,20 +53,23 @@ int main()
     cin >> n;
 
 
-    vector<int> nums(n);
+    vector<int> candidates(n);
 
-    for (int i = 0; i < n; i++)
-        cin >> nums[i];
-
-
-    int k;
-    cin >> k;
+    for(int i = 0; i < n; i++)
+        cin >> candidates[i];
 
 
-    if (canPartitionKSubsets(nums, k))
-        cout << "true";
-    else
-        cout << "false";
+    int target;
+    cin >> target;
+
+
+    Solution s;
+
+
+    vector<vector<int>> res = s.combinationSum(candidates, target);
+
+
+    cout << res.size() << endl;
 
 
     return 0;
